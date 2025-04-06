0b194ea35a1b2d7ef5f01243da474a0d52ddb79d
ScorchHawk <michaelscottsouth@hotmail.co.uk>
2025-04-06 22:01:32 +0100

    Win screen, total XP shown at end of game, sound plays when adjusting SFX volume, debug buttons for winning or losing

--- UI/menu_game_over.gd
+++ UI/menu_game_over.gd
@@ -2,5 +2,14 @@ extends CanvasLayer
 
 signal gameover_menu
 
+func set_victory(state) :
+	if (state) :
+		$PanelContainer/MenuMargin/MenuVBox/GameOver.text = "Victory"
+		$PanelContainer/MenuMargin/MenuVBox/GameOver2.text = "The forces of evil are defeated"
+	else :
+		$PanelContainer/MenuMargin/MenuVBox/GameOver.text = "Game Over"
+		$PanelContainer/MenuMargin/MenuVBox/GameOver2.text = "The forces of evil have prevailed"
+
+
 func _on_button_pressed() -> void:
 	gameover_menu.emit()
